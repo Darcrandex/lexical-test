5282136c60dbf1f728010829f397c4fa433df55d
darcrand <darcrandex@gmail.com>
2023-02-26 02:08:14 +0000

    🦄 refactor: update

--- src/pages/DocEdit/LexicalEditor/plugins/BlockType/BlockTypeSettings.tsx
+++ src/pages/DocEdit/LexicalEditor/plugins/BlockType/BlockTypeSettings.tsx
@@ -6,9 +6,10 @@
 
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
 import { $createHeadingNode, $isHeadingNode } from '@lexical/rich-text'
-import { $setBlocksType_experimental } from '@lexical/selection'
+import { $setBlocksType_experimental, $getSelectionStyleValueForProperty, $patchStyleText } from '@lexical/selection'
 import { Button, Divider, Select, Space } from 'antd'
 import * as lexical from 'lexical'
+import { SketchPicker } from 'react-color'
 import { useCallback, useEffect, useState } from 'react'
 import { useCurrentBlockNode } from '../../utils/use-current-block-node'
 
@@ -26,6 +27,48 @@ export const TextFormatOptions = [
   { value: 'underline', label: 'U' },
   { value: 'code', label: '<>' },
 ]
+
+export const ElementFormatOptions = [
+  { value: 'left', label: '左对齐' },
+  { value: 'center', label: '居中' },
+  { value: 'right', label: '右对齐' },
+  { value: 'justify', label: '两端对齐' },
+]
+
+function useTextStyles() {
+  const [editor] = useLexicalComposerContext()
+  const [color, setColor] = useState('#000')
+
+  const setTextStyles = useCallback(
+    (styles: Record<string, string>) => {
+      editor.update(() => {
+        console.log('ssss', styles)
+        const selection = lexical.$getSelection()
+        console.log('selection', selection)
+
+        if (lexical.$isRangeSelection(selection)) {
+          $patchStyleText(selection, styles)
+        }
+      })
+    },
+    [editor]
+  )
+
+  const onUpdated = useCallback(() => {
+    editor.update(() => {
+      const selection = lexical.$getSelection()
+      if (lexical.$isRangeSelection(selection)) {
+        setColor($getSelectionStyleValueForProperty(selection, 'color', '#000'))
+      }
+    })
+  }, [editor])
+
+  useEffect(() => {
+    return editor.registerUpdateListener(onUpdated)
+  }, [editor, onUpdated])
+
+  return { color, setTextStyles }
+}
 
 export function BlockTypeSettings() {
   const { currBlockNode } = useCurrentBlockNode()
@@ -63,7 +106,7 @@ export function BlockTypeSettings() {
   }, [currBlockNode, currBlockNode?.nodeKey, editor, updateType])
 
   // 修改文本样式
-  const onFormat = useCallback(
+  const onFormatText = useCallback(
     (type: lexical.TextFormatType) => {
       editor.update(() => {
         editor.dispatchCommand(lexical.FORMAT_TEXT_COMMAND, type)
@@ -72,6 +115,19 @@ export function BlockTypeSettings() {
     [editor]
   )
 
+  // 修改块元素对齐方式
+  const onFormatElement = useCallback(
+    (type: lexical.ElementFormatType) => {
+      editor.update(() => {
+        editor.dispatchCommand(lexical.FORMAT_ELEMENT_COMMAND, type)
+      })
+    },
+    [editor]
+  )
+
+  // 基础样式
+  const { color, setTextStyles } = useTextStyles()
+
   if (!currBlockNode || TextBlockTypes.every((t) => t !== currBlockNode.nodeType))
     return <p className='py-4 text-center text-gray-300'>请先选择一个节点</p>
 
@@ -79,13 +135,29 @@ export function BlockTypeSettings() {
     <>
       <Select className='w-40' value={type} onChange={onSetBlockType} options={TextTypeOptions} />
       <Divider />
+
       <Space>
-        {TextFormatOptions.map((v) => (
-          <Button key={v.value} type='link' onClick={() => onFormat(v.value as lexical.TextFormatType)}>
+        {ElementFormatOptions.map((v) => (
+          <Button key={v.value} type='link' onClick={() => onFormatElement(v.value as lexical.ElementFormatType)}>
             {v.label}
           </Button>
         ))}
       </Space>
+      <Divider />
+
+      <Space>
+        {TextFormatOptions.map((v) => (
+          <Button key={v.value} type='link' onClick={() => onFormatText(v.value as lexical.TextFormatType)}>
+            {v.label}
+          </Button>
+        ))}
+      </Space>
+      <Divider />
+
+      <Button onClick={() => setTextStyles({ color: '#fa4526' })}>color</Button>
+
+      {/* 组件点击会让 selection 丢失 */}
+      <SketchPicker color={color} onChangeComplete={(color) => setTextStyles({ color: color.hex })} />
     </>
   )
 }
